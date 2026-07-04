28f3a3ed14add4a675237052d960d1c13fc8e1d6
pvdsonic-bit <pvdsonic@gmail.com>
2026-07-04 19:45:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Lumos Follow
+const magicButton = document.getElementById("magicButton");
+const magicPanel = document.getElementById("magicPanel");
+
+magicButton.addEventListener("click", () => {
+
+    if (magicPanel.style.display === "none") {
+        magicPanel.style.display = "flex";
+    } else {
+        magicPanel.style.display = "none";
+    }
+
+});
+
+document.getElementById("exportButton").addEventListener("click", () => {
+    alert("Modo Exportar Dados será desenvolvido na próxima etapa.");
+});
+
+document.getElementById("loginButton").addEventListener("click", () => {
+    alert("Modo Login com Instagram será desenvolvido na próxima etapa.");
+});
