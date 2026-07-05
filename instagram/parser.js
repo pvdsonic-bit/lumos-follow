603d2397ebf7126cff83257f53b89f76b531ff5d
pvdsonic-bit <pvdsonic@gmail.com>
2026-07-05 10:53:01 -0400

    Adicionar parser do Instagram

--- instagram/parser.js
+++ instagram/parser.js
@@ -1,1 +1,32 @@
-console.log("Parser carregado!");
+// instagram/parser.js
+
+function extrairUsuariosDoHTML(html) {
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+
+    const usuarios = [];
+
+    doc.querySelectorAll("a").forEach(link => {
+
+        const href = link.href;
+
+        if (href.includes("instagram.com")) {
+
+            usuarios.push(
+                limparUsuario(href)
+            );
+
+        }
+
+    });
+
+    return removerDuplicados(usuarios);
+
+}
+
+async function lerArquivoHTML(file) {
+
+    return await file.async("string");
+
+}
