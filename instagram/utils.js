d7a833cef38f9d8dea1c4f0a1d2797fddf408bb5
pvdsonic-bit <pvdsonic@gmail.com>
2026-07-05 10:51:36 -0400

    utils pronto

--- instagram/utils.js
+++ instagram/utils.js
@@ -1,1 +1,20 @@
-console.log("Utils carregado!");
+// instagram/utils.js
+
+function limparUsuario(link) {
+    return link
+        .replace("https://www.instagram.com/", "")
+        .replace("http://www.instagram.com/", "")
+        .replace("www.instagram.com/", "")
+        .replace("instagram.com/", "")
+        .replaceAll("/", "")
+        .trim()
+        .toLowerCase();
+}
+
+function removerDuplicados(lista) {
+    return [...new Set(lista)];
+}
+
+function compararListas(seguindo, seguidores) {
+    return seguindo.filter(usuario => !seguidores.includes(usuario));
+}
